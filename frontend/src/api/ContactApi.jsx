becf6b6f91c8e6f57866971169f69e05a9d06b8e
radheyjhorar <shyamjhorar700@gmail.com>
2025-04-07 08:35:48 +0530

    Using jsonplaceholder api

--- frontend/src/api/ContactApi.jsx
+++ frontend/src/api/ContactApi.jsx
@@ -1,25 +1,30 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:3001"
+  // baseURL: "http://localhost:3001"
+  baseURL: "https://jsonplaceholder.typicode.com"
 })
 
 // get method 
 export const getContacts = () => {
-  return api.get("/contacts")
+  // return api.get("/contacts")
+  return api.get("/users")
 }
 
 // Delete Method 
 export const deleteOneContact = (id) => {
-  return api.delete(`/contacts/${id}`)
+  // return api.delete(`/contacts/${id}`)
+  return api.delete(`/users/${id}`)
 }
 
 // Post Method 
 export const addNewContact = (contact) => {
-  return api.post(`/contacts`, contact);
+  // return api.post(`/contacts`, contact);
+  return api.post(`/users`, contact);
 }
 
 // Put Method
 export const updateContact = (id, data) => {
-  return api.put(`/contacts/${id}`, data);
+  // return api.put(`/contacts/${id}`, data);
+  return api.put(`/users/${id}`, data);
 }
